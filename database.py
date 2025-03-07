55219095f1a6bde709ceffb9b29a67831fe1dd83
Mohammed-Nasr-137 <139057548+Mohammed-Nasr-137@users.noreply.github.com>
2025-03-08 01:28:42 +0200

    database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
-DATABASE_URL = "sqlite:///./sensor_data.db"
+DATABASE_URL = "sqlite:///sensor_data.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
