d1f0ae85d6ba3a843d49bf4a788370fbc915cb9a
Mohammed-Nasr-137 <139057548+Mohammed-Nasr-137@users.noreply.github.com>
2025-03-08 00:34:22 +0200

    run_server.py

--- run_server.py
+++ run_server.py
@@ -1,6 +1,9 @@
 import uvicorn
+import database
 from main import app
 
+
 if __name__ == "__main__":
+    database.init_db()
     uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
     # uvicorn.run("main:app", host="192.168.120.88", port=8000, reload=True)
