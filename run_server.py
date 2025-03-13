6a8c3f1d55758d579e9509978960dfaa3d59623e
Mohammed-Nasr-137 <139057548+Mohammed-Nasr-137@users.noreply.github.com>
2025-03-13 03:28:16 +0200

    run_server.py

--- run_server.py
+++ run_server.py
@@ -4,8 +4,8 @@ from main import app
 from fastapi import FastAPI
 
 # ✅ Initialize database before running the server
-database.init_db()
+
 
 if __name__ == "__main__":
-    
+    database.init_db()
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
