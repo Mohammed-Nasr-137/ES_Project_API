a7fe732deda651216ba4e5ef1f09dbb40bed45c4
Mohammed-Nasr-137 <139057548+Mohammed-Nasr-137@users.noreply.github.com>
2025-04-13 00:16:13 +0200

    Update run_server.py

--- run_server.py
+++ run_server.py
@@ -11,6 +11,6 @@ print("\u2705 Database initialized with tables!")
 
 # Mount the Dash app on a subpath (e.g. /dashboard)
 fastapi_app.mount("/dashboard", WSGIMiddleware(dash_app.server))
-
+app = fastapi_app  # 👈 this is what Koyeb needs
 if __name__ == "__main__":
     uvicorn.run("run_server:fastapi_app", host="0.0.0.0", port=8000, reload=True)
