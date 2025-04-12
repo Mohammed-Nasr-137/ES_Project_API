37208f4efa6c3b14a1966aafb51dd7c5f8c8fa6b
Mohammed-Nasr-137 <139057548+Mohammed-Nasr-137@users.noreply.github.com>
2025-04-12 20:10:23 +0200

    Update run_server.py

--- run_server.py
+++ run_server.py
@@ -8,6 +8,6 @@ database.init_db()
 
 if __name__ == "__main__":
     # uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)  # Corrected
-    uvicorn.run("/frontend/app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
 
     # uvicorn.run("main:app", host="192.168.120.88", port=8000, reload=True)
