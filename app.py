ead2d032ec9f7110d45e5b123be09d92e6d36cbe
Mohammed-Nasr-137 <139057548+Mohammed-Nasr-137@users.noreply.github.com>
2025-04-13 00:05:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ API_URL = "https://shiny-deana-mohammednasr-4c86290b.koyeb.app/sensor_data/1"
 # ---------------------------
 # App Initialization
 # ---------------------------
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
-app.title = "Lake Depth Dashboard"
+dash_app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
+dash_app.title = "Lake Depth Dashboard"
 
 # ---------------------------
 # Helper Functions
